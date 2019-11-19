ee4f68e1d8a81abf9ce478af5eefb4ae96cb4354
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 21:39:17 +0200

    Store the errors in an array for displaying them efficiently without echoing them

--- actions/add-pizza-action.php
+++ actions/add-pizza-action.php
@@ -1,39 +1,41 @@
 <?php
+    $errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
     if(isset($_POST['submit'])) {
         if (empty($_POST['email'])) {
-            echo 'Email is required <br/>';
+            $errors['email'] = 'Email is required <br/>';
         } else {
             $email = $_POST['email'];
             
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo 'Email must be a valid email address';
+                $errors['email'] = 'Email must be a valid email address';
             }
 
-            echo htmlspecialchars($_POST['email']);
+            // echo htmlspecialchars($_POST['email']);
         }
 
         if (empty($_POST['title'])) {
-            echo 'Title is required <br/>';
+            $errors['title'] = 'Title is required <br/>';
         } else {
             $title = $_POST['title'];
 
             if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-                echo 'Title must be letters and spaces only <br/>';
+                $errors['title'] = 'Title must be letters and spaces only <br/>';
             }
 
-            echo htmlspecialchars($_POST['title']);
+            // echo htmlspecialchars($_POST['title']);
         }
 
         if (empty($_POST['ingredients'])) {
-            echo 'Ingredients are required <br/>';
+            $errors['ingredients'] = 'Ingredients are required <br/>';
         } else {
             $ingredients = $_POST['ingredients'];
 
             if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-                echo 'Ingredients must be a comma separated list <br/>';
+                $errors['ingredients'] = 'Ingredients must be a comma separated list <br/>';
             }
 
-            echo htmlspecialchars($_POST['ingredients']);
+            // echo htmlspecialchars($_POST['ingredients']);
         }
     }
 ?>
