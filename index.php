2557c71a3cb265d3aa236cd882526f9f15953eff
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 17:22:38 +0200

    Get rid of unwanted heading and close the PHP tags

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<?php
-?> 
+<?php ?> 
 
 
 <!DOCTYPE html>
 <html lang="en">
     <?php require('templates/header.php') ?>
-    <h1>Hello, World!</h1>
+
     <?php require('templates/footer.php') ?>
 </html>
