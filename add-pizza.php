36c761e86d09f4f5ab5714196fa3c83a8c67cea8
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 21:51:01 +0200

    Prevent the XSS vulnerability

--- add-pizza.php
+++ add-pizza.php
@@ -7,13 +7,13 @@
         <h4 class="center">Add Pizza</h4>
         <form action="add-pizza.php" method="POST" class="white">
             <label for="email">Email:</label>
-            <input type="text" name="email" value="<?php echo $email ?>">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
             <div class="red-text"><?php echo $errors['email']; ?></div>
             <label for="title">Title:</label>
-            <input type="text" name="title" value="<?php echo $title ?>">
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
             <div class="red-text"><?php echo $errors['title']; ?></div>
             <label for="ingredients">Ingredients:</label>
-            <input type="text" name="ingredients" value="<?php echo $ingredients ?>">
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
             <div class="red-text"><?php echo $errors['ingredients']; ?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
