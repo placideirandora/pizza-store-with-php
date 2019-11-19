969ee93cfab630ed481dfb7ec965c6315974fab6
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 19:46:17 +0200

    Require the action for adding pizza

--- add-pizza.php
+++ add-pizza.php
@@ -1,5 +1,4 @@
-<?php ?> 
-
+<?php require('actions/add-pizza-action.php') ?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,13 +6,13 @@
 
     <section class="container grey-text">
         <h4 class="center">Add Pizza</h4>
-        <form action="" method="" class="white">
+        <form action="add-pizza.php" method="POST" class="white">
             <label for="email">Email:</label>
             <input type="email" name="email">
             <label for="title">Title:</label>
             <input type="text" name="title">
             <label for="ingredients">Ingredients:</label>
-            <input type="text" name="ingredients ">
+            <input type="text" name="ingredients">
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
             </div>
