5b780c67bdee0562a7a7f1ee77fc8762900bf978
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 21:47:54 +0200

    Persist data in the form

--- add-pizza.php
+++ add-pizza.php
@@ -7,13 +7,13 @@
         <h4 class="center">Add Pizza</h4>
         <form action="add-pizza.php" method="POST" class="white">
             <label for="email">Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo $email ?>">
             <div class="red-text"><?php echo $errors['email']; ?></div>
             <label for="title">Title:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="<?php echo $title ?>">
             <div class="red-text"><?php echo $errors['title']; ?></div>
             <label for="ingredients">Ingredients:</label>
-            <input type="text" name="ingredients">
+            <input type="text" name="ingredients" value="<?php echo $ingredients ?>">
             <div class="red-text"><?php echo $errors['ingredients']; ?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
