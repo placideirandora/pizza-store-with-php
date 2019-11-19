a7d00c8ca1ad18f2c41756a6bd1daede4f2d8b58
Placide IRANDORA <placideirandora@gmail.com>
2019-11-19 20:22:04 +0200

    Implement server-side validation

--- actions/add-pizza-action.php
+++ actions/add-pizza-action.php
@@ -1,7 +1,21 @@
 <?php
     if(isset($_POST['submit'])) {
-        echo htmlspecialchars($_POST['email']);
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']); 
+        if (empty($_POST['email'])) {
+            echo 'Email is required <br/>';
+        } else {
+            echo htmlspecialchars($_POST['email']);
+        }
+
+        if (empty($_POST['title'])) {
+            echo 'Title is required <br/>';
+        } else {
+            echo htmlspecialchars($_POST['title']);
+        }
+
+        if (empty($_POST['ingredients'])) {
+            echo 'Ingredients are required <br/>';
+        } else {
+            echo htmlspecialchars($_POST['ingredients']);
+        }
     }
 ?>
