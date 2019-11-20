c09a994d753f67ec614d95b25493d8bbd5ba8086
Placide IRANDORA <placideirandora@gmail.com>
2019-11-20 13:25:46 +0200

    Relocate the files

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'credentials.php';
-
-    $conn = mysqli_connect($host, $user, $password, $database);
-
-    if (!$conn) {
-       echo 'database connection error: ' . mysqli_connect_error(); 
-    }
-?>
