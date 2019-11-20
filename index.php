2a7f6387ba85032bb3b61647de1ba8635339f6ac
Placide IRANDORA <placideirandora@gmail.com>
2019-11-20 22:50:09 +0200

    Display a pizza image

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             <?php foreach($pizzas as $pizza): ?>
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
+                        <img src="img/pizza.svg" alt="pizza-image" class="pizza">
                         <div class="card-content center">
                             <h6> 
                                 <?php echo htmlspecialchars($pizza['title']) ?> 
