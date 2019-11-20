e13f4e2ca8040f933ae35e4e00ca624d05be3a20
Placide IRANDORA <placideirandora@gmail.com>
2019-11-20 12:47:01 +0200

    Render the pizzas in the template after a successful retrieval from the database

--- index.php
+++ index.php
@@ -4,6 +4,27 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php require 'templates/header.php' ?>
-
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza) { ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6> 
+                                <?php echo htmlspecialchars($pizza['title']) ?> 
+                            </h6>
+                            <div>
+                                <?php echo htmlspecialchars($pizza['ingredients']) ?> 
+                            </div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">More Info</a>
+                        </div>
+                    </div> 
+                </div>
+            <?php } ?>
+        </div>
+    </div>
     <?php require 'templates/footer.php' ?>
 </html>
