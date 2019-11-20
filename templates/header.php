9bd3fd9a2679d8df47a458066c4fa511721c4e38
Placide IRANDORA <placideirandora@gmail.com>
2019-11-20 13:19:19 +0200

    Link the pages

--- templates/header.php
+++ templates/header.php
@@ -10,12 +10,12 @@
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
         <div class="container">
-            <a href="#" class="brand-logo brand-text">Pizza Store</a>
+            <a href="index.php" class="brand-logo brand-text">Pizza Store</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
                 <li><a href="#" class="btn brand z-depth-0">Menu</a></li>
                 <li><a href="#" class="btn brand z-depth-0">About Us</a></li>
                 <li><a href="#" class="btn brand z-depth-0">Place Order</a></li>
-                <li><a href="#" class="btn brand z-depth-0">Add Pizza</a></li>
+                <li><a href="add-pizza.php" class="btn brand z-depth-0">Add Pizza</a></li>
             </ul>
         </div>
     </nav>
