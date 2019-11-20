d0829b736cff8343661b5d5f9a922005949d06ab
Placide IRANDORA <placideirandora@gmail.com>
2019-11-20 22:50:57 +0200

    Style the pizza image

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="./styles/main.css">
     <title>Pizza Store</title>
+    <style type='text/css'>
+        .pizza {
+        width: 100px;
+        margin: 40px auto -30px;
+        display: block;
+        position: relative;
+        top: -30px; 
+    }
+    </style>
 </head>
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
