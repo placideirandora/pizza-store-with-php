4f525b1c95d0c1ce18d897128d445bf839a8ff22
Placide IRANDORA <placideirandora@gmail.com>
2019-11-21 21:55:44 +0200

    Reuse the code for signing users in, persist data, and display corresponding errors

--- sign-in.php
+++ sign-in.php
@@ -1,13 +1,20 @@
+<?php require 'actions/sign-in-action.php' ?> 
+
 <!DOCTYPE html>
 <html lang="en">
     <?php require 'templates/header.php' ?>
     <section class="container grey-text">
             <h4 class="center">SIGN IN</h4>
+            <div class="<?php echo htmlspecialchars($response['values']); ?>">
+            <?php echo htmlspecialchars($response['result']); ?>
+            </div>
             <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" className="white">
                 <label htmlFor="email">Email</label>
-                <input type="text" name="email" />       
+                <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>" /> 
+                <div class="red-text"><?php echo $errors['email']; ?></div>      
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" />
+                <div class="red-text"><?php echo $errors['password']; ?></div>
                 <p class="center">Don't have an account? <a href="sign-up.php" class="grey-text">SIGNUP</a></p>
                 <br>
                 <div class="center">
